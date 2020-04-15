63f873db545c8c2994ce52b7a29d7eb5210b9470
nanako kan <n.k.0213.0529@gmail.com>
2020-04-15 15:19:22 +0900

    add :投稿一覧画面表示機能の実装

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,10 +1,16 @@
 class TalksController < ApplicationController
   def index
+    @talks = Talk.all
   end
   def new
     @talk = Talk.new
   end
   def create
-    Talk.create(content: params[:talk][:content])
+    Talk.create(talk_params)
+    redirect_to new_talk_path
+  end
+  private
+  def talk_params
+    params.require(:talk).permit(:content)
   end
 end
